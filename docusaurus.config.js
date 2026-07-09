edd319983ededd37846c30ebc393676f42ebfe7a
Emily Allen <emilyallen@Emilys-MacBook-Pro.local>
2026-07-09 15:48:18 -0700

    Set onBrokenLinks to warn while building out content pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'emily-s-allen', // Usually your GitHub org/user name.
   projectName: 'docusaurus-netlify-portfolio', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
